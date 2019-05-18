d9448fa0e04c22f92c5733a61477d73c3764f96b
lovehxl <1039150482@qq.com>
2019-05-18 11:19:43 +0800

    最终版本

--- config/index.js
+++ config/index.js
@@ -21,8 +21,8 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        // host: '192.168.100.29', //192.168.100.43 localhost can be overwritten by process.env.HOST
-        host: "127.0.0.1", //本地测试
+        host: '192.168.100.29', //192.168.100.43 localhost can be overwritten by process.env.HOST
+        // host: "127.0.0.1", //本地测试
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: false,
         errorOverlay: true,
